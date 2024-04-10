8aa2c0a909cdd6d6abf8c93bf8b5cf7f7fdced24
RGRakib <80514518+RGRakib@users.noreply.github.com>
2024-04-10 11:30:55 +0600

    Update index.js
    activity update

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ client.once('ready', () => {
   setTimeout(() => {
     console.log('\x1b[32m%s\x1b[0m', `|    🎯 Activity sucessfully set!`);
     client.user.setPresence({
-      activities: [{ name: `Music with Blood`, type: ActivityType.Listening }],
+      activities: [{ name: `Over Anime community`, type: ActivityType.Watching }],
       status: 'online',
     });
   }, 2000); 
